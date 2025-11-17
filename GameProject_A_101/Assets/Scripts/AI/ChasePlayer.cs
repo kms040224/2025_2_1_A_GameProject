e8f2f9ecda61da2bc04c94f2239514608d7e7098
kms040224 <kms0402246103@gmail.com>
2025-11-17 14:02:46 +0900

    [Feat]: NavMesh 설정하여 캐릭터 이동

---
+++ GameProject_A_101/Assets/Scripts/AI/ChasePlayer.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+
+    public Transform player;                                //유저 위치
+    public float chaseRange = 50.0f;
+    public float attackRange = 2.0f;
+
+    private NavMeshAgent agent;                              //길찾기 알고리즘을 지원 해주는 AI agent
+    private float distanceToPlayer;                         //플레이어와의 거리
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= chaseRange)
+        {
+            ChasePlayers();
+        }
+        else
+        {
+            StopChasing();
+        }
+
+        if(distanceToPlayer <= attackRange)
+        {
+            Attack();
+        }
+    }
+
+    void ChasePlayers()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(player.position);              //플레이어 위치로 목적지 설정
+
+    }
+
+    void StopChasing()
+    {
+        agent.isStopped = true;
+    }
+
+    void Attack()
+    {
+        agent.isStopped = true;
+        transform.LookAt(player);
+        Debug.Log("Attacking Player");
+    }
+
+     void OnDrawGizmosSelected()
+     {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere (transform.position, attackRange);
+     }
+}
